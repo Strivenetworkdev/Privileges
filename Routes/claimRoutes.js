bd2ee6dfce9632598397d205e6fb8afdf06df01a
surfer05 <rahul05alok@gmail.com>
2023-04-04 01:47:28 +0530

    Redeem and Transfer updated

--- Routes/claimRoutes.js
+++ Routes/claimRoutes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const privilegeController = require("../Controllers/privilegeController");
+const claimController = require("../Controllers/claimController")
 
 // Route to transfer a claimed privilege
-router.post("/transfer", privilegeController.transferPrivilege);
+router.post("/transfer", claimController.transferPrivilege);
 
 // Route to redeem a privilege
-router.post("/redeem", privilegeController.redeemPrivilege);
+router.post("/redeem", claimController.redeemPrivilege);
 
 module.exports = router;
